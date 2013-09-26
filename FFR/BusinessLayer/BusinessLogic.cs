2da01bda38525e5d7ef81953afa920b7b1ed9883
nolsen7187 <nolsen@regis.edu>
2013-09-25 21:58:35 -0600

    Added untracked files to potential circumvent reference errors

---
+++ FFR/BusinessLayer/BusinessLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service;
+using FFR;
+
+namespace BusinessLayer
+{
+    public class Facade
+    {
+        //public[
+        public void InstantiateCallerRequested(string CallerRequested)
+        {
+            switch (CallerRequested)
+            {
+                case "Customer":
+                    return;
+                default:
+                    return;
+
+
+            }   
+        }
+    }
+}
